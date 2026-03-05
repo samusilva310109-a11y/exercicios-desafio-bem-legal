fa4ddb754fdf7e931656649d4a38c5e321e9c292
samusilva310109-a11y <samusilva310109@gmail.com>
2026-03-05 13:38:13 -0300

    Exercicio-05: Adicionado contadores de quantidade de números ímpares/pares as funções: "calcPares" e "calcImpares" no arquivo "calculos.js" na pasta "module"

--- exercicio-05/module/calculos.js
+++ exercicio-05/module/calculos.js
@@ -1,20 +1,26 @@
-const calcPares = (numInicial, numFinal)=>{
+const calcPares = (numInicial, numFinal) => {
     console.log('Lista de números pares: ')
-    for(let cont = numInicial; cont <= numFinal; cont++){
-        if(cont % 2 === 0){
-                console.log(cont)
+    let contPares = 0
+    for (let cont = numInicial; cont <= numFinal; cont++) {
+        if (cont % 2 === 0) {
+            console.log(cont)
+            contPares++
         }
     }
+    console.log(`Quantidade de números pares: ${contPares}`)
 }
 
-const calcImapares = (numInicial, numFinal)=>{
+const calcImapares = (numInicial, numFinal) => {
     console.log('Lista de números ímpares: ')
-    for(cont = numInicial; cont <= numFinal; cont++){
-        if(cont % 2 != 0){
+    let contImpares = 0
+    for (let cont = numInicial; cont <= numFinal; cont++) {
+        if (cont % 2 != 0) {
             console.log(cont)
+            contImpares++
         }
     }
+    console.log(`Quantidade de números ímpares: ${contImpares}`)
 }
 
 let pares = calcPares(2, 10)
-let impares = calcImapares(2,10)
+let impares = calcImapares(2, 10)
